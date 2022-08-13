a0025cb29d5e217886e81e4ec0927faf712a3595
Felix <gabritelos@hotmail.com>
2022-08-13 11:43:35 -0300

    Validar Login adicionado

--- src/app/main/login-component/login-component.component.ts
+++ src/app/main/login-component/login-component.component.ts
@@ -15,12 +15,60 @@ export class LoginComponentComponent implements OnInit {
     email: '',
     password: '',
   };
+
+  isGoodLogin: boolean = true;
+  isNotEmpty: boolean = true;
+
   loginRequest(): void {
     console.log(this.userInput);
-    this.userService
-      .login(this.userInput)
-      .subscribe((something) => console.log(something));
+
+    let isPasswordValid: boolean = this.validPassword();
+    let isUsernameValid: boolean = this.validUsername();
+
+    console.log('isPasswordValid value ' + isPasswordValid);
+
+    if (isPasswordValid && isUsernameValid) {
+      this.isNotEmpty = true;
+      this.makeLogin();
+    } else {
+      this.isNotEmpty = false;
+      console.log('\nCredential is NOT valid\n');
+      //show error message
+    }
   }
 
+  makeLogin(): void {
+    this.userService.login(this.userInput).subscribe({
+      next: (msg) => {
+        console.log('Login feito' + msg);
+      },
+      error: (e) => {
+        this.badlogin();
+      },
+    });
+  }
   ngOnInit(): void {}
+
+  badlogin(): void {
+    this.isGoodLogin = false;
+  }
+
+  validPassword(): boolean {
+    const exp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}/g;
+    // const myRegex: RegExp = new RegExp(
+    //   '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,12}$'
+    // );
+    let result = exp.test(this.userInput.password);
+
+    console.log(
+      'teste password passa regex ' + this.userInput.password + ' ' + result
+    );
+
+    return result;
+  }
+
+  validUsername(): boolean {
+    if (this.userInput.email != '') return true;
+    return false;
+  }
 }
